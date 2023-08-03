33f4927a0e79123120dfdb61e2d4723974b797b2
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-08-03 19:41:20 +0530

    small changes

--- src/utils/dbConn.js
+++ src/utils/dbConn.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URL) {
+if (!MONGODB_URI) {
     throw new Error(
         "Please define the MONGODB_URI environment variable inside .env.local"
     )
@@ -27,7 +27,7 @@ const dbConnect = async () => {
             bufferCommands : false
         };
 
-        cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose
         })
     }
